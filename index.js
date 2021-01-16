c4fb4cff17ec0f39eefff7338c28fe42dbb3a24f
ryu-man <rioman19@gmail.com>
2021-01-16 14:45:49 +0100

    Organize code

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path")
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Check if running on production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+} else {
+	require("dotenv").config();
+}
+
 // useFindAndModify to use query like "findOneAupdate"
 mongoose.set("useFindAndModify", false);
+// Connect to the Database
+mongoose.connect(process.env.DB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', ()=>(console.log("connected to Dashu DB...")))
+
 
 // Setup routers
 require("./routes/auth")(app);
@@ -21,30 +35,7 @@ app.use((req, res, next, err) => {
 	}
 });
 
-// Check if running on production
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/public")))
-	app.get('*',(req, res)=>{
-		res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
-	})
-
-} else {
-	require("dotenv").config();
-}
-
 // Start listening
 app.listen(process.env.PORT, () => {
-	// Connect to the Database
-	mongoose
-		.connect(process.env.DB_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
-		.then((db) => {
-			console.log("Dashu db connected...");
-		})
-		.catch((err) => {
-			console.log(err);
-		});
 	console.log("Dashu server started...");
 });
